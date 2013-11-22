9e16c8f148b3a9fa778f148bbe352a6b754f2f7e
seronis <seronis@gmail.com>
2013-11-21 23:18:10 -0500

    Finished requirements for basic echo
    end of line client settings recognized and handled
    sessions now able to recognize zero length commands
    server will echo all received text back to ONLY the originator

--- dev/src/session.cpp
+++ dev/src/session.cpp
@@ -1,15 +1,15 @@
-
 #include <string>
 #include <iostream>
 
 #include "server.h"
 #include "session.h"
 
-Session::Session(Socket * a_sock)
+Session::Session( Socket * a_sock )
 {
 	this->m_pSocket = a_sock;
 	this->m_pServer = a_sock->GetServer();
 	this->state = SessionData::intro_splash;
+	this->eol = "\n";
 }
 
 Session::~Session()
@@ -18,21 +18,22 @@ Session::~Session()
 		if( m_pSocket->IsOpen() ) {
 			//TODO: proper logging
 			std::cerr << "ERROR: session being destructed while socket still open.\n";
-			m_pServer->CloseSocket(m_pSocket);
+			m_pServer->CloseSocket( m_pSocket );
 		}
+
 		delete m_pSocket;
 	}
 }
 
-std::string Session::splash("Hi all!!!\n");
-void Session::SetSplash(std::string msg)
+std::string Session::splash( "Hi all!!!\n" );
+void Session::SetSplash( std::string msg )
 {
 	Session::splash = msg;
 }
 
 void Session::Write( std::string _str )
 {
-	m_pSocket->Write(_str);
+	m_pSocket->Write( _str );
 }
 
 void Session::Process()
@@ -42,62 +43,80 @@ void Session::Process()
 	this->m_pSocket->ClrInBuffer();
 
 	//extract one 'line' from local buffer if we dont have one already
+	bool cmdFound = true;
+
 	if( last_cmd.empty() ) {
-		size_t firstNL = this->inbuffer.find('\n');
-		if( firstNL >= 0 ) {
-			last_cmd = inbuffer.substr(0,firstNL);
-			inbuffer.erase(0,firstNL);
-		}
+		size_t firstNL = this->inbuffer.find( this->eol );
+
+		if( firstNL != std::string::npos ) {
+			last_cmd = inbuffer.substr( 0, firstNL );
+			size_t endNL = firstNL + this->eol.length();
+			inbuffer.erase( 0, endNL );
+		} else
+			cmdFound = false;
 	}
 
 	//actually process command
-	switch( this->state )
-	{
+	switch( this->state ) {
 
-	case SessionData::intro_splash:
-		//TODO: check for banned status. Disconnect or send Splash as appropriate
-		m_pSocket->Write(Session::splash);
-		state = SessionData::login_nameget;
-		return;
+	case SessionData::intro_splash: {
+			//TODO: check for banned status. Disconnect or send Splash as appropriate
+			m_pSocket->Write( Session::splash );
+			m_pSocket->Write( "\nPress [enter] to continue: \n" );
+			state = SessionData::login_detect_eol;
+			return;
+		}
 
-	case SessionData::login_nameget:
-		m_pSocket->Write("Please enter your login name: ");
-		state = SessionData::login_nameck;
-		return;
+	case SessionData::login_detect_eol: {
+			if( cmdFound == false ) {
+				//TODO: timeout after 30 seconds and disconnect socket
+				return;
+			}
 
-	case SessionData::login_nameck:
-		{
-			if( last_cmd.empty() )	return;
+			size_t CREOL = this->last_cmd.find( '\r' );
+
+			if( CREOL != std::string::npos ) {
+				this->eol = "\r\n";
+			}
+
+			last_cmd.clear();
+			state = SessionData::login_nameget;
+			return;
+		}
+
+	case SessionData::login_nameget: {
+			m_pSocket->Write( "Please enter your login name: " );
+			state = SessionData::login_nameck;
+			return;
+		}
+
+	case SessionData::login_nameck: {
+			if( cmdFound == false ) {
+				//TODO: timeout after 30 seconds and disconnect socket
+				return;
+			}
 
 			std::string msg = "\nHi -USERNAME-, we are gonna drop you into Echo mode now.  Enjoy!!\n";
-			int pos = msg.find("USERNAME");
-			msg.replace( pos, 8, last_cmd);
-			m_pSocket->Write(msg);
+			int pos = msg.find( "USERNAME" );
+			msg.replace( pos, 8, last_cmd );
+			m_pSocket->Write( msg );
 			state = SessionData::login_complete;
 			last_cmd.clear();
 			return;
 		}
 
 	case SessionData::login_complete:
-	default:
-		{
-			if( last_cmd.empty() ) return;
-
-			Session *pSession;
-			std::list<Session*>::iterator iSession;
-			std::list<Session*> sessionList = m_pServer->GetSessionList();
-			for( iSession = sessionList.begin(); iSession != sessionList.end(); ) {
-				pSession = *iSession++;
-				std::string msg;
-				msg += "(";
-				msg += std::to_string(last_cmd.length());
-				msg += ") ";
-				msg += last_cmd;
-				pSession->Write(msg);
+	default: {
+			if( cmdFound == false ) {
+				return;
 			}
+
+			this->Write( last_cmd + "\n" );
+
 			last_cmd.clear();
 			break;
 		}
 	}
+
 	return;
 }
--- dev/src/session.h
+++ dev/src/session.h
@@ -7,34 +7,39 @@
 
 class SessionData
 {
-	public:
-		enum state{ intro_splash, login_nameget, login_nameck, login_passget, login_passck, login_complete };
-		SessionData();
-		~SessionData();
-	private:
-		std::string acctname;
-		std::string nickname;
-		std::string password;
+public:
+	enum state { intro_splash, login_detect_eol,
+				 login_nameget, login_nameck,
+				 login_passget, login_passck,
+				 login_complete
+			   };
+	SessionData();
+	~SessionData();
+private:
+	std::string acctname;
+	std::string nickname;
+	std::string password;
 };
 
 class Session
 {
-	public:
-		Session(Socket * a_sock);
-		~Session();
+public:
+	Session( Socket * a_sock );
+	~Session();
 
-		static void SetSplash( std::string msg );
-		void Process( void );
-		void Write( std::string _str );
-	private:
-		static std::string splash;
-		SessionData::state state;
-		Socket * m_pSocket;
-		Server * m_pServer;
-		std::string acctname;	//used as the 'login'. not visible to players
-		std::string nickname;	//visible to other players
-		std::string inbuffer;	//entire input so far received from socket
-		std::string last_cmd;	//latest 'line' popped from inbuffer
+	static void SetSplash( std::string msg );
+	void Process( void );
+	void Write( std::string _str );
+private:
+	static std::string splash;
+	SessionData::state state;
+	Socket * m_pSocket;
+	Server * m_pServer;
+	std::string acctname;	//used as the 'login'. not visible to players
+	std::string nickname;	//visible to other players
+	std::string inbuffer;	//entire input so far received from socket
+	std::string last_cmd;	//latest 'line' popped from inbuffer
+	std::string eol;		//eol sequence used by client
 };
 
 #endif // SESSION_H
